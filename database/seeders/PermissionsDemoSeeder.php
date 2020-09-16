9f5231ea115fc1f0150bc1d207396336186ea2e2
Chris Brown <code@drbyte.dev>
2020-09-16 13:14:43 -0400

    Add example seeder

---
+++ database/seeders/PermissionsDemoSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsDemoSeeder extends Seeder
+{
+    /**
+     * Create the initial roles and permissions.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Role::create(['name' => 'admin']);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example User',
+            'email' => 'test@example.com',
+        ]);
+        $user->assignRole('admin');
+
+        dd($user->roles->pluck('name'));
+
+    }
+}
